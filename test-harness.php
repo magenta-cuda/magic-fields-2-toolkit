eb3848e20db87ed214a52635515a5ecbf19d21bd
magenta-cuda <magenta.cuda@yahoo.com>
2015-09-24 13:26:06 -1000

    verify database queries in test harness

--- test-harness.php
+++ test-harness.php
@@ -3,7 +3,18 @@
 Plugin Name: Test Harness
 */
 add_filter( 'the_content', function( $content ) {
+    function th1_query_filter( $query ) {
+        error_log( '$query=' . $query );
+        return $query;
+    }
+    add_filter( 'query', 'th1_query_filter' );
     $field_id = \mf2tk\get_field_id( 'name', 'colors' );
+    error_log( '$field_id=' . $field_id );
+    $field_id = \mf2tk\get_field_id( 'rgb_code', 'colors' );
+    error_log( '$field_id=' . $field_id );
+    $field_id = \mf2tk\get_field_id( 'hue', 'colors' );
+    error_log( '$field_id=' . $field_id );
+    $field_id = \mf2tk\get_field_id( 'video_test', 'motor' );
     error_log( '$field_id=' . $field_id );
     $group_id = \mf2tk\get_group_id( 'dimensions', 'engine' );
     error_log( '$group_id=' . $group_id );
@@ -17,6 +28,11 @@ add_filter( 'the_content', function( $content ) {
     error_log( '$field_type=' . $field_type );
     $field_options = \mf2tk\get_field_options( 'video_test', 'motor' );
     error_log( '$field_options=' . print_r( $field_options, true ) );
+    $field_options = \mf2tk\get_field_options( 'sound_test', 'motor' );
+    error_log( '$field_options=' . print_r( $field_options, true ) );
+    $field_options = \mf2tk\get_field_options( 'image_media', 'manufacturer' );
+    error_log( '$field_options=' . print_r( $field_options, true ) );
+    remove_filter( 'query', 'th1_query_filter' );
     return $content;
 } );
 ?>
